f0985403c566e61efc3a7e7b751b99e70aed35e0
phongdatpirago <kieukhanhduy1@gmail.com>
2021-08-22 23:18:11 +0700

    [WIP] P2A

---
+++ src/main/java/com/duykk/code/forces/P2A.java
@@ -0,0 +1,20 @@
+package com.duykk.code.forces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+
+public class P2A {
+  static Scanner scanner = new Scanner(System.in);
+  static PrintWriter out = new PrintWriter(System.out);
+
+  public static void main(String[] args) throws IOException {
+    long n = scanner.nextLong();
+    while ()
+    out.close();
+  }
+}
